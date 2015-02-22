0bdad7a5b39ca33794da50d2aa1e6eeba3efd5c6
Roy Zheng <roycraft3@gmail.com>
2015-02-21 22:07:33 -0800

    ok angularized

---
+++ cordova/mapvote/www/js/octagon.js
@@ -0,0 +1,29 @@
+var equitiesApp = angular.module('octagon', ['mobile-angular-ui', 'ngRoute', 'equitiesControllers']);
+/*
+ * 
+equitiesApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+                when('/', {
+                    templateUrl: 'home.html',
+                    controller: 'HomeCtrl'
+                }).
+                when('/top', {
+                    templateUrl: 'top.html',
+                    controller: 'TopCtrl'
+                })
+    }]); 
+ */
+var equitiesControllers = angular.module('equitiesControllers', []);
+equitiesControllers.controller('Home', ['$scope', '$sce', '$routeParams', '$http', function($scope, $sce, $routeParams, $http) {
+        $scope.isLoading = true;
+        $http.get("http://treehacks.cloudapp.net/cheese/god.php?action=setsquares&sun_long=123&sun_lat=32.08")
+                .success(function(data, status, headers, config) {
+                    $scope.faker=data;
+                })
+                .error(function(data, status, headers, config) {
+                    $scope.error = "Connection error";
+                    $scope.isLoading = false;
+                });
+    }
+]);
