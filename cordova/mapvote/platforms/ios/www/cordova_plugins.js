33027b1e07d38cb3b5501d6c895b053372736585
yglazko <yglazko@yglazko.local>
2015-02-22 04:39:31 -0800

    FLJKHGLKSJL

--- cordova/mapvote/platforms/ios/www/cordova_plugins.js
+++ cordova/mapvote/platforms/ios/www/cordova_plugins.js
@@ -41,13 +41,42 @@ module.exports = [
         "clobbers": [
             "cordova.logger"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.camera": "0.3.6-dev",
-    "org.apache.cordova.console": "0.2.14-dev"
+    "org.apache.cordova.console": "0.2.14-dev",
+    "org.apache.cordova.geolocation": "0.3.13-dev"
 }
 // BOTTOM OF METADATA
 });
---
+++ cordova/mapvote/platforms/ios/www/js/octagon.js
@@ -0,0 +1,43 @@
+var equitiesApp = angular.module('octagon', ['mobile-angular-ui', 'ngRoute', 'octagonControllers']);
+/*
+ * 
+ equitiesApp.config(['$routeProvider',
+ function ($routeProvider) {
+ $routeProvider.
+ when('/', {
+ templateUrl: 'home.html',
+ controller: 'HomeCtrl'
+ }).
+ when('/top', {
+ templateUrl: 'top.html',
+ controller: 'TopCtrl'
+ })
+ }]); 
+ */
+var equitiesControllers = angular.module('octagonControllers', []);
+equitiesControllers.controller('HomeCtrl', ['$scope', '$sce', '$routeParams', '$http', function($scope, $sce, $routeParams, $http) {
+        $scope.isLoading = true;
+        $scope.huzzah = function(element) {
+            $http.get("http://treehacks.cloudapp.net/cheese/god.php?action=squareroot&superuniqueuuididentificationidentifier=" + element.superuniqueuuididentificationidentifier)
+                    .success(function(data, status, headers, config) {
+                        element.disabled = true;
+                        element.vector++;
+                    })
+                    .error(function(data, status, headers, config) {
+                    });
+        }
+        $http.get("http://treehacks.cloudapp.net/cheese/god.php?action=setsquares&sun_long=123&sun_lat=32.08")
+                .success(function(data, status, headers, config) {
+                    data.forEach(function(element, index, array) {
+                        element.disabled = false;
+                    });
+                    $scope.faker = data;
+                    $scope.isLoading = false;
+                    console.log(data);
+                })
+                .error(function(data, status, headers, config) {
+                    $scope.error = "Connection error";
+                    $scope.isLoading = false;
+                });
+    }
+]);
