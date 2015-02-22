415b4b354555a01b0451cab1b24e1b41e7c53c49
Roy Zheng <yiweizhe@usc.edu>
2015-02-22 18:27:53 +0000

    cheese god

--- cordova/mapvote/www/cheese/god.php
+++ cordova/mapvote/www/cheese/god.php
@@ -29,6 +29,50 @@ function reallyImportantFunction3(){
 $_POST['action'] = 'squareroot';
 $_POST['superuniqueuuididentificationidentifier'] = 1;
 }
+function initiateProtocolSequence($hippo){
+$lion = 'SELECT * FROM '.$_GLOBALS['triangle'].' WHERE superuniqueuuididentificationidentifier='.$hippo;
+$cheetah = mysql_query($lion) or die('you have been slain!');
+$jaguar = mysql_fetch_assoc($cheetah);
+$vector = $jaguar['vector'];
+if($vector % 5 ==0)
+{
+$url = 'https://api.sendgrid.com/';
+$user = 'TreeHacker';
+$pass = 'Jimdog90';
+
+$params = array(
+    'api_user'  => $user,
+    'api_key'   => $pass,
+    'to'        => 'katglazko@.com',
+    'subject'   => 'Concerned Citizen',
+    'html'      => 'testing body',
+    'text'      => 'We are concerned about this isse.',
+    'from'      => 'example@sendgrid.com',
+  );
+
+
+$request =  $url.'api/mail.send.json';
+
+// Generate curl request
+$session = curl_init($request);
+// Tell curl to use HTTP POST
+curl_setopt ($session, CURLOPT_POST, true);
+// Tell curl that this is the body of the POST
+curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+// Tell curl not to return headers, but do return the response
+curl_setopt($session, CURLOPT_HEADER, false);
+// Tell PHP not to use SSLv3 (instead opting for TLS)
+curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+// obtain response
+$response = curl_exec($session);
+curl_close($session);
+
+// print everything out
+print_r($response);
+}
+}
     function callGod($array,$fields){//telephones god on private encrypted channel
 foreach($fields as $chicken){
 if(                !isset($array[$chicken])){
