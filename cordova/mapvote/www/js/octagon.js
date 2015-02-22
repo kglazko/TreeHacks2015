b8305545fe034722ec6c8e5da031857ce298691e
Roy Zheng <roycraft3@gmail.com>
2015-02-21 22:51:54 -0800

    silly post mistake

--- cordova/mapvote/www/js/octagon.js
+++ cordova/mapvote/www/js/octagon.js
@@ -18,9 +18,9 @@ var equitiesControllers = angular.module('octagonControllers', []);
 equitiesControllers.controller('HomeCtrl', ['$scope', '$sce', '$routeParams', '$http', function($scope, $sce, $routeParams, $http) {
         $scope.isLoading = true;
         $scope.huzzah = function(element) {
-            $http.post("http://treehacks.cloudapp.net/cheese/god.php?action=squareroot&'superuniqueuuididentificationidentifier=" + element.superuniqueuuididentificationidentifier)
+            $http.post("http://treehacks.cloudapp.net/cheese/god.php", { action:squareroot,superuniqueuuididentificationidentifier:element.superuniqueuuididentificationidentifier})
                     .success(function(data, status, headers, config) {
-                        element.disabled=true;
+                        element.disabled = true;
                         element.vector++;
                     })
                     .error(function(data, status, headers, config) {
@@ -28,8 +28,8 @@ equitiesControllers.controller('HomeCtrl', ['$scope', '$sce', '$routeParams', '$
         }
         $http.get("http://treehacks.cloudapp.net/cheese/god.php?action=setsquares&sun_long=123&sun_lat=32.08")
                 .success(function(data, status, headers, config) {
-                    data.forEach(function(element,index,array){
-                       element.disabled=false; 
+                    data.forEach(function(element, index, array) {
+                        element.disabled = false;
                     });
                     $scope.faker = data;
                     $scope.isLoading = false;
