1826c2db06cc13286f39bc059edacd487ddea219
Roy Zheng <roycraft3@gmail.com>
2015-02-21 22:42:11 -0800

    buttonpress

--- cordova/mapvote/www/js/octagon.js
+++ cordova/mapvote/www/js/octagon.js
@@ -1,26 +1,39 @@
 var equitiesApp = angular.module('octagon', ['mobile-angular-ui', 'ngRoute', 'octagonControllers']);
 /*
  * 
-equitiesApp.config(['$routeProvider',
-    function ($routeProvider) {
-        $routeProvider.
-                when('/', {
-                    templateUrl: 'home.html',
-                    controller: 'HomeCtrl'
-                }).
-                when('/top', {
-                    templateUrl: 'top.html',
-                    controller: 'TopCtrl'
-                })
-    }]); 
+ equitiesApp.config(['$routeProvider',
+ function ($routeProvider) {
+ $routeProvider.
+ when('/', {
+ templateUrl: 'home.html',
+ controller: 'HomeCtrl'
+ }).
+ when('/top', {
+ templateUrl: 'top.html',
+ controller: 'TopCtrl'
+ })
+ }]); 
  */
 var equitiesControllers = angular.module('octagonControllers', []);
 equitiesControllers.controller('HomeCtrl', ['$scope', '$sce', '$routeParams', '$http', function($scope, $sce, $routeParams, $http) {
         $scope.isLoading = true;
+        $scope.huzzah = function(element) {
+            $http.post("http://treehacks.cloudapp.net/cheese/god.php?action=squareroot&superuniqueuuididentificationidentifier=" + element.superuniqueuuididentificationidentifier)
+                    .success(function(data, status, headers, config) {
+                        element.disabled=true;
+                        row++;
+                    })
+                    .error(function(data, status, headers, config) {
+                    });
+            row++;
+        }
         $http.get("http://treehacks.cloudapp.net/cheese/god.php?action=setsquares&sun_long=123&sun_lat=32.08")
                 .success(function(data, status, headers, config) {
-                    $scope.faker=data;
-                    $scope.isLoading=false;
+                    data.forEach(function(element,index,array){
+                       element.disabled=false; 
+                    });
+                    $scope.faker = data;
+                    $scope.isLoading = false;
                     console.log(data);
                 })
                 .error(function(data, status, headers, config) {
